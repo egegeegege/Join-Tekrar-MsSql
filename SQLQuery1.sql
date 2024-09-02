08588d903ffb65b4f039d18d85fa703c056a58da
Ege Aydın <151860320+egegeegege@users.noreply.github.com>
2024-09-02 18:21:43 +0300

    SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,8 +1,8 @@
 use HastaneDB
 
--- 201 nolu polikliniktek çalýþan hasta ve hemþireleri göster
-select p.Poliklinik_Adi ,d.Adi AS 'Doktor Adý',d.Soyadi AS 
-'Doktor Soyadý',h.Adi AS 'Hemþire Adý',h.Soyadi AS 'Hemþire Soyadý'from Poliklinikler p 
+-- 201 nolu polikliniktek Ã§alÄ±ÅŸan hasta ve hemÅŸireleri gÃ¶ster
+select p.Poliklinik_Adi ,d.Adi AS 'Doktor AdÄ±',d.Soyadi AS 
+'Doktor SoyadÄ±',h.Adi AS 'HemÅŸire AdÄ±',h.Soyadi AS 'HemÅŸire Soyadi'from Poliklinikler p 
 join Hemsireler h on p.ID = h.Poliklinik_ID
 join Doktorlar d on p.ID = d.Poliklinik_ID
 
@@ -18,17 +18,17 @@ where h.Adi = 'Ali' and h.Soyadi = 'Demir'
 select * from hastalar
 select * from Randevular
 
--- Ergüç ailesini getiren sorgu
+-- ErgÃ¼Ã§ ailesini getiren sorgu
 select h.Adi,h.Soyadi,h.Sigorta_Turu,r.Randevu_Durumu,Randevu_Tarihi from hastalar h
 join Randevular r on h.ID = r.Hasta_ID
-where h.Soyadi like '%Ergüç%'
+where h.Soyadi like '%ErgÃ¼Ã§%'
 
 select * from Personel
 select * from Doktorlar
 
--- Orhan çýnar adlý doktorun ünvanýný getir
+-- Orhan Ã§Ä±nar adlÄ± doktorun Ã¼nvanÄ±nÄ± getir
 select d.Adi,d.Soyadi,p.Unvan from Doktorlar d join Personel p on d.Personel_ID = p.ID
-where d.Adi = 'Orhan' and d.Soyadi = 'Çýnar'
+where d.Adi = 'Orhan' and d.Soyadi = 'Ã‡Ä±nar'
 
 select * from Poliklinikler
 
@@ -39,10 +39,10 @@ select h.Adi,h.Soyadi,b.Bolum_Adi,b.Bulundugu_Blok
 from Hemsireler h
 join Bolumler b on h.Bolum_ID = b.ID
 
---orhan çýnar adlý doktorun verdiði randevu ve tahlil sonuçlarýnýn durumunu gösterme
+--orhan Ã§Ä±nar adlÄ± doktorun verdiÄŸi randevu ve tahlil sonuÃ§larÄ±nÄ±n durumunu gÃ¶sterme
 
-select  d.Adi as 'Doktor Adý', d.Soyadi as 'Doktor Soyadý',
+select  d.Adi as 'Doktor AdÄ±', d.Soyadi as 'Doktor SoyadÄ±',
 r.Randevu_Durumu,t.Tahlil_Adi,t.Tahlil_Sonuc from Doktorlar d 
 join Randevular r on d.ID = r.Doktor_ID
 join Tahliller t on d.ID = t.Doktor_ID
-where d.Adi = 'Orhan' and d.Soyadi = 'Çýnar'
+where d.Adi = 'Orhan' and d.Soyadi = 'Ã‡Ä±nar'
